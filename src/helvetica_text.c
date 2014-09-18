983369b38bb7b75f569229a43a08a209fbca2abb
Jason Puglisi <jason@puglisi.im>
2014-09-17 20:44:26 -0400

    Release 2.0: - Changed minute font to Helvetica Light - Decreased font sizes - Condensed text vertically - Made minor update changes

--- src/helvetica_text.c
+++ src/helvetica_text.c
@@ -4,188 +4,186 @@ Window *window;
 InverterLayer *inverter_layer;
 
 TextLayer *time1_text_layer;
-char time1_buffer[] = "xxxxxx";
+char time1_buffer[] = "      ";
 TextLayer *time2_text_layer;
-char time2_buffer[] = "xxxxxxxx";
+char time2_buffer[] = "        ";
 TextLayer *time3_text_layer;
-char time3_buffer[] = "xxxxx";
+char time3_buffer[] = "     ";
 TextLayer *date_text_layer;
-char date_buffer[] = "xxxxxxxxxx";
+char date_buffer[] = "          ";
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  if (units_changed & MINUTE_UNIT) {
-    int hour = tick_time->tm_hour;
-    switch (hour % 12) {
+  int hour = tick_time->tm_hour;
+  switch (hour % 12) {
+    case 0:
+      strcpy(time1_buffer, "twelve");
+      break;
+    case 1:
+      strcpy(time1_buffer, "one");
+      break;
+    case 2:
+      strcpy(time1_buffer, "two");
+      break;
+    case 3:
+      strcpy(time1_buffer, "three");
+      break;
+    case 4:
+      strcpy(time1_buffer, "four");
+      break;
+    case 5:
+      strcpy(time1_buffer, "five");
+      break;
+    case 6:
+      strcpy(time1_buffer, "six");
+      break;
+    case 7:
+      strcpy(time1_buffer, "seven");
+      break;
+    case 8:
+      strcpy(time1_buffer, "eight");
+      break;
+    case 9:
+      strcpy(time1_buffer, "nine");
+      break;
+    case 10:
+      strcpy(time1_buffer, "ten");
+      break;
+    case 11:
+      strcpy(time1_buffer, "eleven");
+      break;
+  }
+  text_layer_set_text(time1_text_layer, time1_buffer);
+  
+  int min = tick_time->tm_min;
+  int ten = min / 10;
+  int one = min % 10;
+  if (ten == 0) {
+    switch (one) {
       case 0:
-        strcpy(time1_buffer, "twelve");
+        strcpy(time2_buffer, "o'clock");
         break;
       case 1:
-        strcpy(time1_buffer, "one");
+        strcpy(time2_buffer, "o'one");
         break;
       case 2:
-        strcpy(time1_buffer, "two");
+        strcpy(time2_buffer, "o'two");
         break;
       case 3:
-        strcpy(time1_buffer, "three");
+        strcpy(time2_buffer, "o'three");
         break;
       case 4:
-        strcpy(time1_buffer, "four");
+        strcpy(time2_buffer, "o'four");
         break;
       case 5:
-        strcpy(time1_buffer, "five");
+        strcpy(time2_buffer, "o'five");
         break;
       case 6:
-        strcpy(time1_buffer, "six");
+        strcpy(time2_buffer, "o'six");
         break;
       case 7:
-        strcpy(time1_buffer, "seven");
+        strcpy(time2_buffer, "o'seven");
         break;
       case 8:
-        strcpy(time1_buffer, "eight");
+        strcpy(time2_buffer, "o'eight");
         break;
       case 9:
-        strcpy(time1_buffer, "nine");
-        break;
-      case 10:
-        strcpy(time1_buffer, "ten");
-        break;
-      case 11:
-        strcpy(time1_buffer, "eleven");
+        strcpy(time2_buffer, "o'nine");
         break;
     }
-    text_layer_set_text(time1_text_layer, time1_buffer);
-    
-    int min = tick_time->tm_min;
-    int ten = min / 10;
-    int one = min % 10;
-    if (ten == 0) {
-      switch (one) {
-        case 0:
-          strcpy(time2_buffer, "o'clock");
-          break;
-        case 1:
-          strcpy(time2_buffer, "o'one");
-          break;
-        case 2:
-          strcpy(time2_buffer, "o'two");
-          break;
-        case 3:
-          strcpy(time2_buffer, "o'three");
-          break;
-        case 4:
-          strcpy(time2_buffer, "o'four");
-          break;
-        case 5:
-          strcpy(time2_buffer, "o'five");
-          break;
-        case 6:
-          strcpy(time2_buffer, "o'six");
-          break;
-        case 7:
-          strcpy(time2_buffer, "o'seven");
-          break;
-        case 8:
-          strcpy(time2_buffer, "o'eight");
-          break;
-        case 9:
-          strcpy(time2_buffer, "o'nine");
-          break;
-      }
-      strcpy(time3_buffer, "");
-    } else if (ten == 1) {
-      switch (one) {
-        case 0:
-          strcpy(time2_buffer, "ten");
-          strcpy(time3_buffer, "");
-          break;
-        case 1:
-          strcpy(time2_buffer, "eleven");
-          strcpy(time3_buffer, "");
-          break;
-        case 2:
-          strcpy(time2_buffer, "twelve");
-          strcpy(time3_buffer, "");
-          break;
-        case 3:
-          strcpy(time2_buffer, "thirteen");
-          strcpy(time3_buffer, "");
-          break;
-        case 4:
-          strcpy(time2_buffer, "four");
-          strcpy(time3_buffer, "teen");
-          break;
-        case 5:
-          strcpy(time2_buffer, "fifteen");
-          strcpy(time3_buffer, "");
-          break;
-        case 6:
-          strcpy(time2_buffer, "sixteen");
-          strcpy(time3_buffer, "");
-          break;
-        case 7:
-          strcpy(time2_buffer, "seven");
-          strcpy(time3_buffer, "teen");
-          break;
-        case 8:
-          strcpy(time2_buffer, "eight");
-          strcpy(time3_buffer, "teen");
-          break;
-        case 9:
-          strcpy(time2_buffer, "nine");
-          strcpy(time3_buffer, "teen");
-          break;
-      }
-    } else {
-      switch (ten) {
-        case 2:
-          strcpy(time2_buffer, "twenty");
-          break;
-        case 3:
-          strcpy(time2_buffer, "thirty");
-          break;
-        case 4:
-          strcpy(time2_buffer, "forty");
-          break;
-        case 5:
-          strcpy(time2_buffer, "fifty");
-          break;
-      }
-      switch (one) {
-        case 0:
-          strcpy(time3_buffer, "");
-          break;
-        case 1:
-          strcpy(time3_buffer, "one");
-          break;
-        case 2:
-          strcpy(time3_buffer, "two");
-          break;
-        case 3:
-          strcpy(time3_buffer, "three");
-          break;
-        case 4:
-          strcpy(time3_buffer, "four");
-          break;
-        case 5:
-          strcpy(time3_buffer, "five");
-          break;
-        case 6:
-          strcpy(time3_buffer, "six");
-          break;
-        case 7:
-          strcpy(time3_buffer, "seven");
-          break;
-        case 8:
-          strcpy(time3_buffer, "eight");
-          break;
-        case 9:
-          strcpy(time3_buffer, "nine");
-          break;
-      }
+    strcpy(time3_buffer, "");
+  } else if (ten == 1) {
+    switch (one) {
+      case 0:
+        strcpy(time2_buffer, "ten");
+        strcpy(time3_buffer, "");
+        break;
+      case 1:
+        strcpy(time2_buffer, "eleven");
+        strcpy(time3_buffer, "");
+        break;
+      case 2:
+        strcpy(time2_buffer, "twelve");
+        strcpy(time3_buffer, "");
+        break;
+      case 3:
+        strcpy(time2_buffer, "thirteen");
+        strcpy(time3_buffer, "");
+        break;
+      case 4:
+        strcpy(time2_buffer, "fourteen");
+        strcpy(time3_buffer, "");
+        break;
+      case 5:
+        strcpy(time2_buffer, "fifteen");
+        strcpy(time3_buffer, "");
+        break;
+      case 6:
+        strcpy(time2_buffer, "sixteen");
+        strcpy(time3_buffer, "");
+        break;
+      case 7:
+        strcpy(time2_buffer, "seven");
+        strcpy(time3_buffer, "teen");
+        break;
+      case 8:
+        strcpy(time2_buffer, "eight");
+        strcpy(time3_buffer, "teen");
+        break;
+      case 9:
+        strcpy(time2_buffer, "nine");
+        strcpy(time3_buffer, "teen");
+        break;
     }
-    text_layer_set_text(time2_text_layer, time2_buffer);
-    text_layer_set_text(time3_text_layer, time3_buffer);
+  } else {
+    switch (ten) {
+      case 2:
+        strcpy(time2_buffer, "twenty");
+        break;
+      case 3:
+        strcpy(time2_buffer, "thirty");
+        break;
+      case 4:
+        strcpy(time2_buffer, "forty");
+        break;
+      case 5:
+        strcpy(time2_buffer, "fifty");
+        break;
+    }
+    switch (one) {
+      case 0:
+        strcpy(time3_buffer, "");
+        break;
+      case 1:
+        strcpy(time3_buffer, "one");
+        break;
+      case 2:
+        strcpy(time3_buffer, "two");
+        break;
+      case 3:
+        strcpy(time3_buffer, "three");
+        break;
+      case 4:
+        strcpy(time3_buffer, "four");
+        break;
+      case 5:
+        strcpy(time3_buffer, "five");
+        break;
+      case 6:
+        strcpy(time3_buffer, "six");
+        break;
+      case 7:
+        strcpy(time3_buffer, "seven");
+        break;
+      case 8:
+        strcpy(time3_buffer, "eight");
+        break;
+      case 9:
+        strcpy(time3_buffer, "nine");
+        break;
+    }
   }
+  text_layer_set_text(time2_text_layer, time2_buffer);
+  text_layer_set_text(time3_text_layer, time3_buffer);
   
   if (units_changed & DAY_UNIT) {
     char wday_buffer[4];
@@ -262,32 +260,32 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 void window_load(Window *window) {
-  time1_text_layer = text_layer_create(GRect(1, 0, 143, 64));
+  time1_text_layer = text_layer_create(GRect(2, 10, 142, 64));
   text_layer_set_background_color(time1_text_layer, GColorClear);
   text_layer_set_text_color(time1_text_layer, GColorBlack);
-  text_layer_set_font(time1_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_BOLD_42)));
+  text_layer_set_font(time1_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_BOLD_38)));
   text_layer_set_text(time1_text_layer, time1_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) time1_text_layer);
   
-  time2_text_layer = text_layer_create(GRect(1, 40, 143, 64));
+  time2_text_layer = text_layer_create(GRect(2, 48, 142, 64));
   text_layer_set_background_color(time2_text_layer, GColorClear);
   text_layer_set_text_color(time2_text_layer, GColorBlack);
-  text_layer_set_font(time2_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_42)));
+  text_layer_set_font(time2_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_LIGHT_38)));
   text_layer_set_text(time2_text_layer, time2_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) time2_text_layer);
   
-  time3_text_layer = text_layer_create(GRect(1, 80, 143, 64));
+  time3_text_layer = text_layer_create(GRect(2, 84, 142, 64));
   text_layer_set_background_color(time3_text_layer, GColorClear);
   text_layer_set_text_color(time3_text_layer, GColorBlack);
-  text_layer_set_font(time3_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_42)));
+  text_layer_set_font(time3_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_LIGHT_38)));
   text_layer_set_text(time3_text_layer, time3_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) time3_text_layer);
   
-  date_text_layer = text_layer_create(GRect(0, 135, 138, 30));
+  date_text_layer = text_layer_create(GRect(0, 139, 142, 30));
   text_layer_set_background_color(date_text_layer, GColorClear);
   text_layer_set_text_color(date_text_layer, GColorBlack);
   text_layer_set_text_alignment(date_text_layer, GTextAlignmentRight);
-  text_layer_set_font(date_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_24)));
+  text_layer_set_font(date_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_22)));
   text_layer_set_text(date_text_layer, date_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) date_text_layer);
   
@@ -295,7 +293,7 @@ void window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), (Layer*) inverter_layer);
   
   time_t temp = time(NULL);
-  tick_handler(localtime(&temp), MINUTE_UNIT | DAY_UNIT);
+  tick_handler(localtime(&temp), DAY_UNIT);
 }
 
 void window_unload(Window *window) {
